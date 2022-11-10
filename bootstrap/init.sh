321e4b3c8a5b55ae46fc955ab61ab98dc617e6ed
Alberto Guzman <guzmanelmalo@google.com>
2022-11-10 16:33:12 +0000

    Updated bootstrap to change default location and projectID

--- bootstrap/init.sh
+++ bootstrap/init.sh
@@ -1,6 +1,6 @@
 # Initilize set project ID variable and run various initializations
 # ACTION REQUIRED! Change "project-id-here" value to the project you'll be using
-export PROJECT_ID="project-id-here"
+export PROJECT_ID="guzmanelmalo-test1"
 # sets the current project for gcloud
 gcloud config set project $PROJECT_ID
 # Enables various APIs you'll need
@@ -8,9 +8,9 @@ gcloud services enable container.googleapis.com cloudbuild.googleapis.com \
 artifactregistry.googleapis.com clouddeploy.googleapis.com \
 cloudresourcemanager.googleapis.com
 # creates the Artifact Registry repo
-gcloud artifacts repositories create hello-world-python --location=us-central1 \
+gcloud artifacts repositories create hello-world-python --location=europe-west1 \
 --repository-format=docker
 # creates the Google Cloud Deploy pipeline
 gcloud beta deploy apply --file clouddeploy.yaml \
---region=us-central1 --project=$PROJECT_ID
+--region=europe-west1 --project=$PROJECT_ID
 echo "init done. To create clusters, run: ./gke-cluster-init.sh"
